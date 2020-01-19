2ce1562d0278aa4702a999112c8cfda696948d5f
printempw <printempw@gmail.com>
2020-01-19 22:20:11 +0800

    Fix processing lazy-load images many times

--- scripts/lazyload.js
+++ scripts/lazyload.js
@@ -10,8 +10,12 @@ if (!config.lazyload || !config.lazyload.enable) {
 hexo.extend.filter.register('after_render:html', html => {
   const loading = url_for(config.lazyload.loading_image);
 
-  return html.replace(
-    /(<img[^>]*) src="(.*?)"/gim,
-    `$1 src="$2" srcset="${loading}" data-srcset="$2"`
-  );
+  return html.replace(/(<img[^>]*) src="(.*?)"(.*?>)/gim, (match, p1, p2, p3) => {
+    // Do nothing if already processed
+    if (match.includes(loading)) {
+      return match;
+    }
+
+    return `${p1} src="${p2}" srcset="${loading}" data-srcset="${p2}"${p3}`;
+  });
 });
